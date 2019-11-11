23f154dfac1ad16729b882ee33c829ce9acd2fc7
loki <loki@pop-os.localdomain>
2019-11-11 16:03:46 -0600

    meh

--- p5.pl
+++ p5.pl
@@ -1,14 +1,19 @@
-sat_with_print([H|T]) :-
-  write("Hello World").
 
+sat_with_print(M) :- member([],M).
 
-sat([]).
+sat_with_print(M) :-
+    M=[[L|_]|_], (-N=L;-L=N) ->
+    reduce(M,L,N,M1), sat_with_print(M1),
+    reduce(M,N,L,M2), sat_with_print(M2).
 
+reduce([],_,_,[]).
 
-elim_pos_clauses([]).
-elim_pos_clauses([], 1).
-elim_pos_clauses([],1, 2).
+reduce([C|M],L,N,M1) :-
+    ( member(N,C) -> M1=M2 ;
+    del(L,C,D), M1=[D|M2] ),
+    reduce(M,L,N,M2).
 
-elim_neg_literal([]).
-elim_neg_literal([],1).
-elim_neg_literal([],1,2).
+del(_,[],[]).
+
+del(A,[B|C],D) :-
+    (A=B -> D=E ; D=[B|E]), del(A,C,E).
